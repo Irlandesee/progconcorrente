0405c052b5618627db00b26b4123c62e0d9d3ae8
Mattia Lunardi <mattia.lunardi@gmail.com>
2021-03-15 10:12:06 +0100

    Completed es6

--- esercizi/ThreadBase/es6/Main.java
+++ esercizi/ThreadBase/es6/Main.java
@@ -2,22 +2,29 @@ import java.util.Scanner;
 
 public class Main{
 
-	public static void main(String[] args) throws InterruptedException{
+	public static void main(String[] args){
 		int n;
 		Scanner in = new Scanner(System.in);
 		Solution s;
 		while((n = in.nextInt()) != 0){
 			if(n > 0 && n < 6){
-				s = new Solution(n);
 				for(int i = 0; i < n; i++){
-					System.out.println("Istanza del Thread: "+i);
-					s.start();	
-					
-					System.out.println("Aspetto che questo thread muoia");
-					s.join();
-				}
+					s = new Solution(n);
+					Thread t = new Thread(s);
+					try{
 
+						System.out.println("Istanza del Thread: "+i);
+						t.start();	
+					
+						System.out.println("Aspetto che questo thread muoia");
+						t.join();
+					}catch(InterruptedException e){
+						e.printStackTrace();
+					}
+
+				}
 			}
+
 		}
 	}
 
