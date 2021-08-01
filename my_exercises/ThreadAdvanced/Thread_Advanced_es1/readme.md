5ac4836e7bcd0c0cab7d00fa88a71616051fded8
Mattia Lunardi <mattia.lunardi@gmail.com>
2021-08-01 18:46:50 +0200

    scritti metodi per multithreading, run() quasi completo
    run() non è ancora completo, si deve mettere a dormire il produttore quando il buffer è pieno e svegliarlo quando ci sono posti liberi ecc usando interrupt e notify.

--- my_exercises/ThreadAdvanced/Thread_Advanced_es1/readme.md
+++ my_exercises/ThreadAdvanced/Thread_Advanced_es1/readme.md
@@ -5,3 +5,39 @@ Realizzare un sistema con un produttore e un consumatore:
 -> La cella è thread-safe, ma non blocca i consumatori su buffer vuoto o i produttori su buffer pieno.
 
 => Si osservano problemi
+
+
+# Monitor:
+A monitor is something a thread can grab and hold, preventing all other threads from grabbing that same monitor and forcing them to wait until the monitor is released. This is what a synchronized block does.
+
+# esercizio 1b
+
+modificare la soluzione dell'esercizio 1 in modo che il sistema si comporti <<bene>>, cioè
+-> Il produttore aspetta quando il buffer è pieno
+-> Il consumatore aspetta quando il buffer è vuoto
+
+# Esercizio 2
+Si realizzi un sistema con più produttori e più consumatori.
+
+
+# notes
+
+producer :
+se ce almeno 1 posto libero nel buffer
+	produci un item
+	inseriscilo
+	continua
+altrimenti
+	dormi
+
+consumer :
+se ce almeno 1 posto occupato nel buffer
+	prendi un item
+	consumalo
+	continua
+altrimenti
+	dormi
+
+# misc
+Thread local random:
+ThreadLocalRandom is a combination of the ThreadLocal and Random classes (more on this later) and is isolated to the current thread
--- my_exercises/ThreadAdvanced/Thread_Advanced_es1/solution/Producer.java
+++ my_exercises/ThreadAdvanced/Thread_Advanced_es1/solution/Producer.java
@@ -2,29 +2,110 @@ package solution;
 
 import java.util.Random;
 
-public class Producer{
+public class Producer extends Thread{
 
-	private int bound; 
-	private Random rand;
-	private int[] a;
+	private int boundNumbers; 
+	private int boundIndex;
+	private Random rand; // con piu produttori forse è possibile r.c su rand
+	private Solution s;
+	private boolean runCondition;
 
-	public Producer(int bound, int[] a){
-		this.bound = bound;
-		this.a = a;
-		rand = new Random(bound);
+	public Producer(int boundNumbers, Solution s){
+		this.boundNumbers = boundNumbers;
+		rand = new Random();
+		this.s = s;
+		runCondition = true; //at creation runCondition is always true
+		this.start();
 	}
 
-	private int next(){
-		return rand.nextInt();
+	public void run(){
+		while(runCondition){ //r.c. on runCondition (?)
+			
+			int item = produceRandomItem();
+			int index = produceRandomIndex();
+
+			//start of cs
+			int[] bufferCopy = getBufferCopy();
+
+			//inserting item in the buffer
+			bufferCopy = insert(index, item, bufferCopy);
+			//copying the new buffer in the old buffer
+			setBufferCopy(bufferCopy);
+			//end of cs
+
+			runCondition = getRunCondition(); //checks if it still needs to run
+			printPositions();
+		}
 	}
 
-	public boolean insert(){
-		int item = next();
-		System.out.println(toString() + item);
-		
+	private synchronized int[] getBufferCopy(){
+		return s.getBuffer();
+	}
+
+	private synchronized void setBufferCopy(int[] a){
+		s.setBuffer(a);
+	}
+
+	/**
+	 * if the pos at a[index] is "open"/"free" then the 
+	 * item is insert, otherwise the array is returned 
+	 * a[index] == -1 -> pos is open
+	 * a[index] == 5 -> pos is closed
+	 * Not Thread Safe
+	 * */
+	private synchronized int[] insert(int index, int item, int[] a){
+		if(a[index] == -1){
+			a[index] = item;
+			System.out.printf("Item: %d, inserted at -> %d\n", item, index);
+		}
+		else
+			System.out.println("Position is closed!");
+		return a;
+	}
+
+	public synchronized void printPositions(int[] a){
+		String closed = "closed";
+		String open = "open";
+		System.out.println("[PRINTING POSITIONS]");
+		for(int i = 0; i < a.length; i++){
+			if(a[i] < 0) //position is open
+				System.out.printf("index: %d -> %s\n", i, open);
+			else
+				System.out.printf("index: %d -> value: %d, %s\n", i, a[i], closed);
+		}
+		System.out.println("[DONE]");
+	}
+
+	//Not Thread Safe (?)
+	private int produceRandomIndex(){
+		return rand.nextInt(boundIndex);
+	}
+
+	//Not Thread Safe (?)
+	private int produceRandomItem(){
+		return rand.nextInt(boundNumbers);
+	}
+
+	public int getBoundNumbers(){
+		return this.boundNumbers;
+	}
+
+	public int getBoundIndex(){
+		return this.boundIndex;
+	}
+
+	public boolean getRunCondition(){
+		return this.runCondition;
+	}
+
+	public void setRunCondition(boolean runCondition){
+		this.runCondition = runCondition;
 	}
 
 	public String toString(){
-		return "Producer : ";
+		return "Producer: "
+		+ "\nboundNumbers -> " + this.getBoundNumbers()
+		+ "\nbound index -> " + this.getBoundIndex();
 	}
+
 }
